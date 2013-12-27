e869d1faf43576552eac875e2f0f85688c87a60a
rubcuevas <rubencuevas93@gmail.com>
2013-12-27 13:41:51 +0100

    Initial Commit

---
+++ EllipseDetection/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  EllipseDetection
+//
+//  Created by Rubén Cuevas on 27/12/13.
+//  Copyright (c) 2013 Rubén Cuevas Menéndez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
